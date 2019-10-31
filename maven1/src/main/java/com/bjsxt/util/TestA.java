31a20c89e10e4c3698b53f9aaf50126d3a0f2d08
fll <1904886604@qq.com>
2019-10-31 21:27:18 +0800

    Initial commit

---
+++ maven1/src/main/java/com/bjsxt/util/TestA.java
@@ -0,0 +1,8 @@
+package com.bjsxt.util;
+
+public class TestA {
+    public void test01(){
+        System.out.println("test01");
+    }
+}
+
---
+++ maven2/src/main/java/com/bjsxt/uu/TestB.java
@@ -0,0 +1,11 @@
+package com.bjsxt.uu;
+
+import com.bjsxt.util.TestA;
+
+public class TestB {
+    public static void main(String[] args) {
+        System.out.println("main");
+        TestA testA = new TestA();
+        testA.test01();
+    }
+}
